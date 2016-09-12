c71275910a246741cebe89b20cbdd5d7d58fc23b
Pavel Mavrichev <pavel.mavrichev@gmail.com>
2016-09-12 15:46:58 +0700

    actions as objects. long-term planning. no line movement, Lee algorithm instead

--- src/actions.py
+++ src/actions.py
@@ -16,71 +16,119 @@ class MovementXY(Action):
         self.target_x = None
         self.target_y = None
 
-        self.x_increment = None
-        self.y_increment = None
-        self.straight = None
+        self.path = []
 
-        # y = a + bx
-        self.a = None
-        self.b = None
+    def initialize_path(self):
+        field_map = self.__make_map()
+        self.__wave(field_map, self.subject.x, self.subject.y, self.target_x, self.target_y)
+
+        if field_map[self.target_y][self.target_x] == -1:
+            self.path = []
+
+        self.path = self.__find_backwards(field_map, self.target_x, self.target_y)
+
+    @staticmethod
+    def __wave(field_map, x1, y1, x2, y2):
+        current_wave_list = [(x1, y1)]
+        field_map[y1][x1] = 0
+
+        while len(current_wave_list) > 0 and field_map[y2][x2] is None:
+            next_wave_list = []
+            for coordinates in current_wave_list:
+                x, y = coordinates
+                wave_num = field_map[y][x] + 1
+
+                if (len(field_map)-1 >= y+1) and field_map[y+1][x] is None:
+                    field_map[y+1][x] = wave_num
+                    next_wave_list.append((x, y+1))
+
+                if (y > 0) and field_map[y-1][x] is None:
+                    field_map[y-1][x] = wave_num
+                    next_wave_list.append((x, y-1))
+
+                if (len(field_map[y])-1 >= x+1) and field_map[y][x+1] is None:
+                    field_map[y][x+1] = wave_num
+                    next_wave_list.append((x+1, y))
+
+                if (x > 0) and field_map[y][x-1] is None:
+                    field_map[y][x-1] = wave_num
+                    next_wave_list.append((x-1, y))
+
+            current_wave_list = next_wave_list[:]
+
+    @staticmethod
+    def __find_backwards(field_map, x2, y2):
+        num_steps = field_map[y2][x2]
+
+        if num_steps is None or num_steps == -1:
+            return None
+
+        path = [(x2, y2)]
+        num_steps -= 1
+
+        while num_steps > 0:
+
+            x, y = path[-1]
+
+            if (len(field_map) - 1 >= y + 1) and (field_map[y + 1][x] == num_steps):
+                path.append((x, y + 1))
+            elif (y > 0) and (field_map[y - 1][x] == num_steps):
+                path.append((x, y - 1))
+            elif (len(field_map[y]) - 1 >= x + 1) and (field_map[y][x + 1] == num_steps):
+                path.append((x + 1, y))
+            elif (x > 0) and (field_map[y][x - 1] == num_steps):
+                path.append((x - 1, y))
+
+            num_steps -= 1
+
+        path.reverse()
+
+        return path
+
+    def __make_map(self):
+        field_map = []
+
+        for input_row in self.subject.board.field:
+            row = []
+            for cell in input_row:
+                if cell[-1].passable:
+                    row.append(None)
+                else:
+                    row.append(-1)
+            field_map.append(row)
+
+        return field_map
+
+    def check_path_passable(self):
+
+        for step_coordinates in self.path:
+            if not self.subject.board.field[step_coordinates[1]][step_coordinates[0]][-1].passable:
+                return False
+
+        return True
+
 
     def set_xy(self, x, y):
         self.target_x = x
         self.target_y = y
-
-    def initialize_equation(self):
-        self.x_increment = 0
-        self.y_increment = 0
-
-        y_direction = self.target_y - self.subject.y
-        x_direction = self.target_x - self.subject.x
-
-        if y_direction > 0:
-            self.y_increment = 1
-        elif y_direction < 0:
-            self.y_increment = -1
-
-        if x_direction > 0:
-            self.x_increment = 1
-        elif x_direction < 0:
-            self.x_increment = -1
-
-        if self.x_increment == 0 or self.y_increment == 0:
-            self.straight = True
-        else:
-            self.straight = False
-
-            x1, y1 = float(self.subject.x), float(self.subject.y)
-            x2, y2 = float(self.target_x), float(self.target_y)
-
-            self.b = (y2 - y1) / (x2 - x1)
-            self.a = y1 - (self.b * x1)
 
     def do(self):
         self.check_set_accomplishment()
         if self.accomplished:
             return True
 
-        if self.straight is None \
-                or (self.subject.x == self.target_x or self.subject.y == self.target_y):
-            self.initialize_equation()
+        if not self.path or not self.check_path_passable():
+            self.initialize_path()
 
-        if self.straight:
-            current_step_x = self.subject.x + self.x_increment
-            current_step_y = self.subject.y + self.y_increment
-        else:
-            y = self.a + (self.b * (self.subject.x + self.x_increment))
+        if not self.path:
+            return self.accomplished
 
-            if abs(self.subject.y) <= y <= (abs(self.subject.y) + 1):
-                current_step_x = self.subject.x + self.x_increment
-                current_step_y = self.subject.y
-            else:
-                current_step_x = self.subject.x
-                current_step_y = self.subject.y + self.y_increment
+        current_step_x, current_step_y = self.path[0]
 
         if self.subject.board.field[current_step_y][current_step_x][-1].passable:
             self.subject.board.field[self.subject.y][self.subject.x].pop()
             self.subject.board.insert_object(current_step_x, current_step_y, self.subject, epoch=1)
+            self.path.pop(0)
 
         self.check_set_accomplishment()
 
---
+++ src/temp.py
@@ -0,0 +1,74 @@
+
+def wave(field, x1, y1, x2, y2):
+    current_wave_list = [(x1, y1)]
+    field[y1][x1] = 0
+
+    while len(current_wave_list) > 0 and field[y2][x2] is None:
+        next_wave_list = []
+        for coordinates in current_wave_list:
+            x, y = coordinates
+            wave_num = field[y][x] + 1
+
+            if (len(field)-1 >= y+1) and field[y+1][x] is None:
+                field[y+1][x] = wave_num
+                next_wave_list.append((x, y+1))
+
+            if (y > 0) and field[y-1][x] is None:
+                field[y-1][x] = wave_num
+                next_wave_list.append((x, y-1))
+
+            if (len(field[y])-1 >= x+1) and field[y][x+1] is None:
+                field[y][x+1] = wave_num
+                next_wave_list.append((x+1, y))
+
+            if (x > 0) and field[y][x-1] is None:
+                field[y][x-1] = wave_num
+                next_wave_list.append((x-1, y))
+
+        current_wave_list = next_wave_list[:]
+
+def find_backwards(field, x2, y2):
+    num_steps = field[y2][x2]
+
+    if num_steps is None or num_steps == -1:
+        return None
+
+    path = [(x2, y2)]
+    num_steps -= 1
+
+    while num_steps > 0:
+
+        x, y = path[-1]
+
+        if (len(field) - 1 >= y + 1) and (field[y + 1][x] == num_steps):
+            path.append((x, y + 1))
+        elif (y > 0) and (field[y-1][x] == num_steps):
+            path.append((x, y - 1))
+        elif (len(field[y])-1 >= x+1) and (field[y][x+1] == num_steps):
+            path.append((x + 1, y))
+        elif (x > 0) and (field[y][x-1] == num_steps):
+            path.append((x - 1, y))
+
+        num_steps -= 1
+
+    path.reverse()
+
+    return path
+
+def find_way(field, x1, y1, x2, y2):
+
+    if field[y2][x2] == -1:
+        return None
+
+    wave(field, x1, y1, x2, y2)
+    return find_backwards(field, x2, y2)
+
+
+field = [[None, None, None, None, None],
+         [-1, -1, -1, -1, None],
+         [None, None, None, None, None],
+         [None, -1, -1, -1, -1],
+         [None, None, 0, None, None],
+         [None, None, None, None, None]]
+
+print find_way(field, 2, 4, 3, 0)
